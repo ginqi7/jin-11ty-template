f69322e26511f80a8c917f1826d2ef4c2bce08b1
Qiqi Jin <ginqi7@gmail.com>
2025-04-03 19:36:47 +0800

    feat: add a personal thoughts page.

--- .eleventy.js
+++ .eleventy.js
@@ -1,90 +1,101 @@
-import fs from 'fs';
-import path from 'path';
-import Fontmin from 'fontmin';
+import fs from "fs";
+import path from "path";
+import Fontmin from "fontmin";
 import CleanCSS from "clean-css";
 
-export default function(eleventyConfig) {
-    
+export default function (eleventyConfig) {
     // copy static files to output directory.
     eleventyConfig.addPassthroughCopy("static/*");
     eleventyConfig.addPassthroughCopy("static/fonts/en/*");
     eleventyConfig.addPassthroughCopy("static/svg/*");
-    
+
     // add some language collections.
     listPostLangs().forEach((lang) => {
-	eleventyConfig.addCollection(lang, (collectionApi) => {
-	    return collectionApi.getFilteredByGlob(`posts/${lang}/*`)
-	});
-	eleventyConfig.addCollection(`${lang}TagsList`, (collectionApi) => {
-	    const tagsSet = new Set()
-	    collectionApi.getFilteredByGlob(`posts/${lang}/*`).forEach((item) => {
-		if (!item.data.tags) return
-		item.data.tags.forEach((tag) => tagsSet.add(tag))
-	    })
-	    return Array.from(tagsSet)
-	})
+        eleventyConfig.addCollection(lang, (collectionApi) => {
+            return collectionApi.getFilteredByGlob(`posts/${lang}/*`);
+        });
+        eleventyConfig.addCollection(`${lang}TagsList`, (collectionApi) => {
+            const tagsSet = new Set();
+            collectionApi
+                .getFilteredByGlob(`posts/${lang}/*`)
+                .forEach((item) => {
+                    if (!item.data.tags) return;
+                    item.data.tags.forEach((tag) => tagsSet.add(tag));
+                });
+            return Array.from(tagsSet);
+        });
+        eleventyConfig.addCollection(`${lang}Thoughts`, (collectionApi) => {
+            return collectionApi
+                .getFilteredByGlob(`thoughts/${lang}/*`)
+                .sort(
+                    (a, b) =>
+                        new Date(b.data.create_date) -
+                        new Date(a.data.create_date),
+                );
+        });
     });
-    
+
     // minify css
     minifyCss();
     // minify chinese fonts.
     minifyChineseFonts();
-
-};
+}
 
 function minifyChineseFonts() {
-    const zhPostsDirectory = 'posts/zh/'; 
-    const dataDirectory = "_data/"
-    const content = removeDuplicateCharacters
-    (extractChineseChars
-     (readFilesInDirectory(zhPostsDirectory)
-      + readFilesInDirectory(dataDirectory)
-     ))
+    const zhPostsDirectory = "posts/zh/";
+    const zhThoughtsDirectory = "thoughts/zh/";
+    const dataDirectory = "_data/";
+    const content = removeDuplicateCharacters(
+        extractChineseChars(
+            readFilesInDirectory(zhPostsDirectory) +
+                readFilesInDirectory(zhThoughtsDirectory) +
+                readFilesInDirectory(dataDirectory),
+        ),
+    );
     const fontmin = new Fontmin()
-	  .src('static/fonts/zh/*')
-	  .use(Fontmin.glyph({
-              text: content,
-              hinting: false         
-	  }))
-	  .dest('_site/static/fonts/zh/');
-    
+        .src("static/fonts/zh/*")
+        .use(
+            Fontmin.glyph({
+                text: content,
+                hinting: false,
+            }),
+        )
+        .dest("_site/static/fonts/zh/");
+
     fontmin.run(function (err, files) {
-	if (err) {
+        if (err) {
             throw err;
-	}
+        }
     });
 }
-
 
 function minifyCss() {
-    const sourceCssDir = 'static/css/'
-    const targetCssDir = '_site/static/css/'
+    const sourceCssDir = "static/css/";
+    const targetCssDir = "_site/static/css/";
     if (!fs.existsSync(targetCssDir)) {
-	fs.mkdirSync(targetCssDir, {recursive : true});
+        fs.mkdirSync(targetCssDir, { recursive: true });
     }
     const files = fs.readdirSync(sourceCssDir);
-    files.forEach(file => {
-	var inputFile = sourceCssDir + file
-	var input = fs.readFileSync(inputFile, 'utf8');
-	var output = new CleanCSS().minify(input);
-	fs.writeFile(targetCssDir + file, output.styles, function (err) {
-            if (err) return console.log('Error minifying main.css' + err);
-	});
+    files.forEach((file) => {
+        var inputFile = sourceCssDir + file;
+        var input = fs.readFileSync(inputFile, "utf8");
+        var output = new CleanCSS().minify(input);
+        fs.writeFile(targetCssDir + file, output.styles, function (err) {
+            if (err) return console.log("Error minifying main.css" + err);
+        });
     });
 }
 
-
 function listPostLangs() {
-    return getAllDirectories("posts")
+    return getAllDirectories("posts");
 }
-
 
 function getAllDirectories(directoryPath) {
     let directories = [];
 
     const files = fs.readdirSync(directoryPath);
 
-    files.forEach(file => {
+    files.forEach((file) => {
         const filePath = path.join(directoryPath, file);
         const stat = fs.statSync(filePath);
 
@@ -96,28 +107,27 @@ function getAllDirectories(directoryPath) {
     return directories;
 }
 
-
 function readFilesInDirectory(directoryPath) {
     try {
-	const files =  fs.readdirSync(directoryPath);
-	var content = "";
-	for (let file of files) {
-            const filePath = directoryPath + file
-            const data =  fs.readFileSync(filePath, 'utf8');
-	    content += data
-	}
-	return content
+        const files = fs.readdirSync(directoryPath);
+        var content = "";
+        for (let file of files) {
+            const filePath = directoryPath + file;
+            const data = fs.readFileSync(filePath, "utf8");
+            content += data;
+        }
+        return content;
     } catch (err) {
-	console.error('Error reading directory:', err);
+        console.error("Error reading directory:", err);
     }
 }
 
 function extractChineseChars(str) {
     const regex = /[\u4E00-\u9FFF]/g;
     const chineseChars = str.match(regex);
-    return chineseChars ? chineseChars.join('') : '';
+    return chineseChars ? chineseChars.join("") : "";
 }
 
 function removeDuplicateCharacters(str) {
-    return [...new Set(str)].join('');
+    return [...new Set(str)].join("");
 }
--- _data/icons.js
+++ _data/icons.js
@@ -2,31 +2,46 @@ import project from "./project.js";
 
 export default function () {
     return [
-	{
-	    "type" : "github",
-	    "src" : "/static/svg/github.svg",
-	    "href" : project.github,
-	    "show" : "(Github)"
-	},
-	{
-	    "type" : "email",
-	    "src" : "/static/svg/mail.svg",
-	    "href" : "mailto:" + project.mail,
-	    "show" : "(Email)"
-	},
-	
-	{
-	    "type" : "en",
-	    "src" : "/static/svg/languages.svg",
-	    "href" : "/",
-	    "show" : "(English)"
-	},
-	{
-	    "type" : "zh",
-	    "src" : "/static/svg/languages.svg",
-	    "href" : "/zh",
-	    "show" : "(Chinese)"
-	}
-	
-    ]
-};
+        {
+            type: "github",
+            src: "/static/svg/github.svg",
+            href: project.github,
+            show: "(Github)",
+        },
+        {
+            type: "email",
+            src: "/static/svg/mail.svg",
+            href: "mailto:" + project.mail,
+            show: "(Email)",
+        },
+        {
+            type: "en-language",
+            src: "/static/svg/languages.svg",
+            href: "/",
+            lang: "zh",
+            show: "(English)",
+        },
+        {
+            type: "zh-language",
+            src: "/static/svg/languages.svg",
+            href: "/zh",
+            lang: "en",
+            show: "(Chinese)",
+        },
+        {
+            type: "en-thoughts",
+            src: "/static/svg/thoughts.svg",
+            href: "/en/thoughts",
+            lang: "en",
+            show: "(thoughts)",
+        },
+        {
+            type: "zh-thoughts",
+            src: "/static/svg/thoughts.svg",
+            href: "/zh/thoughts",
+            lang: "zh",
+            show: "(thoughts)",
+        },
+
+    ];
+}
